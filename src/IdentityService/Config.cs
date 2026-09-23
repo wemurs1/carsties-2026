35b8c2c253028b8e596688cb6768f6a3299a5493
Stuart Murray <stuart.murray.dev@outlook.com>
2026-09-23 08:44:04 +0100

    Checkpoint - identity service support for the client

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -15,15 +15,25 @@ public static class Config
         new ApiScope("auctionApp")
     ];
 
-    public static IEnumerable<Client> Clients =>
+    public static IEnumerable<Client> Clients(IConfiguration config) =>
     [
         new()
         {
             ClientId = "bruno",
             ClientName = "Bruno Client",
-            AllowedScopes = {"openid", "profile", "auctionApp"},
-            AllowedGrantTypes =  GrantTypes.ResourceOwnerPassword,
-            ClientSecrets =  {new Secret("NotASecret".Sha256())},
+            AllowedScopes = { "openid", "profile", "auctionApp" },
+            AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+            ClientSecrets = { new Secret("NotASecret".Sha256()) },
+        },
+        new()
+        {
+            ClientId = "nextApp",
+            ClientName = "NextApp Client",
+            AllowedScopes = { "openid", "profile", "auctionApp" },
+            AllowedGrantTypes = GrantTypes.Code,
+            ClientSecrets = { new Secret("NotASecret".Sha256()) },
+            RedirectUris = { config["ClientAppUrl"] + "/api/oauth2/callback/duende" },
+            AccessTokenLifetime = 3600 * 24 * 30
         }
     ];
 }
